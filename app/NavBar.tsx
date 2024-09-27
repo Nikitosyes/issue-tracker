64b4ef5720824fae2145fdaa72540a6cb6e9ad2f
Nick <nick.sieriebriakov@gmail.com>
2024-09-27 22:55:42 +0300

    working with NavBar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,7 +1,12 @@
+"use client";
+import classNames from "classnames";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { IoBug } from "react-icons/io5";
 
 const NavBar = () => {
+  const currentPath = usePathname();
+
   const links = [
     {
       label: "Dashboard",
@@ -15,14 +20,19 @@ const NavBar = () => {
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
       <Link href="/">
-        <IoBug />
+        <IoBug className="h-7 w-7 hover:scale-125 transition-transform " />
       </Link>
       <ul className="flex space-x-6">
         {links.map((l) => (
           <Link
             key={l.href}
-            className="text-zinc-600 transition-colors hover:cursor-pointer hover:text-zinc-900 hover:underline"
-            href="/"
+            className={classNames({
+              "text-black hover:text-zinc-900 font-bold":
+                l.href === currentPath,
+              "text-zinc-600": l.href !== currentPath,
+              " transition-colors hover:cursor-pointer  hover:underline": true,
+            })}
+            href={l.href}
           >
             {l.label}
           </Link>
